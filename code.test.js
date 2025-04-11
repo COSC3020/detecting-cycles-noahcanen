a8419a4ec74c5e412a70b718ac1fe8b680e54a8b
noahcanen <ncanen@uwyo.edu>
2025-04-11 11:54:10 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,3 +2,28 @@ const fs = require('fs');
 const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
+
+var test1 = [[1],[2],[3],[4],[5],[0]]
+
+assert(hasCycle(test1) == true)
+
+var test2 = [[1],[2],[3],[4],[5],[]]
+
+assert(hasCycle(test2) == false)
+
+var test3 = [[1,4,5],[6],[1,3],[],[],[1],[2]]
+
+assert(hasCycle(test3) == true)
+
+
+var test4 = [[1],[2],[],[1]]
+
+assert(hasCycle(test4) == false)
+
+var test5 = []
+
+assert(hasCycle(test5) == false)
+
+var test6 = [[]]
+
+assert(hasCycle(test6) == false)
