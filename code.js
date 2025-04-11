6d583f2e7ce5bc84991cdaf15b240a0e27c559f9
noahcanen <ncanen@uwyo.edu>
2025-04-11 11:53:53 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function hasCycle(graph) {
+    for (let i = 0; i < graph.length; i++) {
+        if (checkNode(i, [],graph)) { return true; };
+    }
+    return false;
+}
+function checkNode(node, seen, graph) {
+    if (seen.includes(node)) { return true; }
+    seen.push(node);
+    for (let j = 0; j < graph[node].length; j++) {
+        if (checkNode(graph[node][j], [...seen],graph)) { return true; }
+    }
     return false;
 }
